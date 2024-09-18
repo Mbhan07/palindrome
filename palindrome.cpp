7d5512ee0ce98370515788779822dcce8653260a
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-09-18 10:11:59 -0700

    Add files via upload
    tgus us tge= actual updated palinformd [rojecty

--- palindrome.cpp
+++ palindrome.cpp
@@ -3,10 +3,21 @@
 
 using namespace std;
 
+
+//geeksforgeeks https://www.geeksforgeeks.org/removing-punctuations-given-string/ 
+
 int main(){
+  char str[80]; //type of array and space (5 chars)
+  cin.get(str, 20);
+  cout << str << endl;
+
+  
+  cout << removePunctuations(str);
+  
+
+  // A NULL CHARACTER IS A \0
 
   //read in a series of characters
-
   //remove all spaces and punctuation
 
   //check if input is the same backwards as is forwards
@@ -18,4 +29,20 @@ int main(){
   return 0;
 
 
+}
+
+string removePunctuations(string palindrome){
+  string result = "";
+  for (char c : palindrome) {
+    if (!ispunct(c)){
+      result += c;
+    }
+  }
+  palindrome = result;
+  return palindrome;
+}
+
+bool isPalindrome(string palindrome){
+
+  return false;
 }
