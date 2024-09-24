ac5d4c72ccf4eef15631baae55d2778053af3b13
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-09-24 10:07:08 -0700

    Add files via upload
    second commiy

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <stdio.h>https://www2.chemistry.msu.edu/faculty/reusch/virttxtjml/physprop.htm
 
 using namespace std;
 
@@ -11,10 +12,10 @@ int main(){
   cin.get(str, 80);
   cout << str << endl;
   int length = strlen(str);
-  cout << "Length is \t" << length << endl;
+  cout << "Length is " << length << endl;
 
   for (int i = 0; i < length; i++){
-    
+    int result = strcmp()
   }
   // A NULL CHARACTER IS A \0
 
